d172c1264ebcf3abc4579463701e4c39deb0587f
xxShawn <275503088@qq.com>
2018-03-29 00:26:55 +0800

    ceshi..

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const Koa = require('koa');
+const koaStatic = require('koa-static');
 var app = new Koa();
+
+
+app.use(koaStatic('./assets/'));
 app.use(async ctx => {
 	ctx.body = 'hello world!'
 })
